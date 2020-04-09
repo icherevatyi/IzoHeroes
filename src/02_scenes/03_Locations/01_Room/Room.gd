19dbbe6a252c2309b2cb141d701b7339c0732532
Igor <igor.cherevatyi@gmail.com>
2020-04-09 21:48:00 +0300

    updated room generation

---
+++ src/02_scenes/03_Locations/01_Room/Room.gd
@@ -0,0 +1,4 @@
+extends Node
+
+var min_x: int = 0
+var min_y: int = 0
--- src/02_scenes/03_Locations/Dungeon.gd
+++ src/02_scenes/03_Locations/Dungeon.gd
@@ -1,25 +1,58 @@
 extends Node2D
 
+enum TILE_TYPE {Empty = -1, Filled = 0}
 
-var size_x: int
-var size_y: int
+const MAP_WIDTH: int = 80
+const MAP_HEIGHT: int = 80
+const MIN_ROOM_SIZE: int = 12
+const MAX_ROOM_SIZE: int = 17
 
-onready var _tilemap_node: TileMap = $TileMap
+var Room: GDScript = preload("res://src/02_scenes/03_Locations/01_Room/Room.gd")
+
+var _rooms: Array = []
+var _map: Array = []
+
+var rng = RandomNumberGenerator.new()
+
+onready var _tile_map: TileMap = $TileMap
+
 
 
 func _ready() -> void:
-	_generate_tile()
+	_generate_map()
 
 
-func _randomize_tile_size() -> void:
-	randomize()
-	size_x = randi() % 15 + 8 
-	size_y = randi() % 15 + 8
-
-func _generate_tile() -> void:
-	_randomize_tile_size()
+func _generate_map() -> void:
+	_map = _create_2d_arr()
 	
-	for element_x in size_x:
-		for element_y in size_y:
-			_tilemap_node.set_cell(element_x, element_y, 0)
-	_tilemap_node.update_bitmask_region()
+	rng.randomize()
+	var _room_width: int = rng.randi_range(MIN_ROOM_SIZE, MAX_ROOM_SIZE)
+	var _room_height: int = rng.randi_range(MIN_ROOM_SIZE, MAX_ROOM_SIZE)
+	
+	for x in _room_width:
+		for y in _room_height:
+			_map[x][y] = TILE_TYPE.Filled
+
+	for x in MAP_WIDTH:
+		for y in MAP_HEIGHT:
+			_tile_map.set_cell(x, y, _map[x][y])
+
+	_tile_map.update_bitmask_region()
+
+
+func _create_2d_arr() -> Array:
+	var _map_array: Array = []
+	
+	for x in MAP_WIDTH:
+		_map_array.append([])
+		
+		for y in MAP_HEIGHT:
+			_map_array[x].append(TILE_TYPE.Empty)
+	
+	return _map_array
+
+
+func _input(event) -> void:
+	if event.is_action_pressed("ui_accept"):
+		_tile_map.clear()
+		_generate_map()
