a6e937781a53c794cd20b33629ce301456209446
icherevatyi <igor.cherevatyi@gmail.com>
2020-06-06 07:22:38 +0300

    added randomized exit door

--- src/02_scenes/03_Locations/02_Dungeon_Scenes/02_Rooms/Room.gd
+++ src/02_scenes/03_Locations/02_Dungeon_Scenes/02_Rooms/Room.gd
@@ -1,6 +1,27 @@
 extends TileMap
 
+var exit_door: PackedScene = preload("res://src/02_scenes/03_Locations/01_Helper_Scenes/ExitDoor.tscn")
+var rng: RandomNumberGenerator = RandomNumberGenerator.new()
 onready var dungeon: Node2D = get_node("/root/Dungeon")
+
+
+
+func _on_create_exit_command_received() -> void:
+	_create_exit(_select_random_wall_tile())
+
+
+func _select_random_wall_tile() -> Vector2:
+	var possible_placement_options = get_used_cells_by_id(02)
+	var viable_placement_options = possible_placement_options.slice(1, possible_placement_options.size() - 2)
+	rng.randomize()
+	var placement = rng.randi_range(0, viable_placement_options.size() - 1)
+	return viable_placement_options[placement]
+
+
+func _create_exit(placement_position: Vector2) -> void:
+	var exit_instance: Node2D = exit_door.instance()
+	exit_instance.set_global_position(placement_position * Vector2(16, 16))
+	add_child(exit_instance)
 
 
 func _connect_signal(signal_title: String, target_node, target_function_title: String) -> void:
--- src/02_scenes/03_Locations/02_Dungeon_Scenes/Dungeon.gd
+++ src/02_scenes/03_Locations/02_Dungeon_Scenes/Dungeon.gd
@@ -1,11 +1,28 @@
 extends Node2D
 
+var rng: RandomNumberGenerator = RandomNumberGenerator.new()
 
 onready var room_container: Node2D = $Rooms
+
+signal _create_exit
 
 
 func _ready() -> void:
 	Backdrop.fade_out()
+	yield(get_tree().create_timer(2),"timeout")
+	_select_random_room()
+	emit_signal("_create_exit")
+
+
+func _select_random_room() -> void:
+	var rooms = room_container.get_children()
+	var selected_child
+	rng.randomize()
+	if rooms.size() > 4:
+		selected_child = rng.randi_range(3, rooms.size() - 1)
+	else:
+		selected_child = rooms.size()
+	_connect_signal("_create_exit", rooms[selected_child], "_on_create_exit_command_received")
 
 
 func _connect_signal(signal_title: String, target_node, target_function_title: String) -> void:
