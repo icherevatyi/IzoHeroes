3c552eec5fea7b28ec3bb2cce88a7eaca66e011d
icherevatyi <igor.cherevatyi@gmail.com>
2020-07-10 08:18:54 +0300

    removed argument for add_key

--- src/02_scenes/01_UI/04_Console/CommandHandler.gd
+++ src/02_scenes/01_UI/04_Console/CommandHandler.gd
@@ -13,7 +13,7 @@ onready var player = get_node("/root/Dungeon/YSort/Player")
 var command_list: Dictionary = {
 	0: {
 		"command": "add_key",
-		"arguments": [ARG_INT] 
+		"arguments": [] 
 	},
 	1: {
 		"command": "add_potion",
@@ -22,7 +22,7 @@ var command_list: Dictionary = {
 }
 
 
-func add_key(arg) -> void:
+func add_key() -> void:
 	var spawn_position: Vector2 = player.get_global_position()
 	var item_instance = Lists.boss_loot_list["key"].scene.instance()
 	item_instance.item_info = Lists.boss_loot_list["key"]
