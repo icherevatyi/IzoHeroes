de55404b041b60ead0c26e019e23f37ba0d3e99c
icherevatyi <igor.cherevatyi@gmail.com>
2020-08-26 21:01:28 +0300

    fixed death animation

--- src/02_scenes/03_Locations/01_Helper_Scenes/EnemySpawnPoint.gd
+++ src/02_scenes/03_Locations/01_Helper_Scenes/EnemySpawnPoint.gd
@@ -13,9 +13,9 @@ func get_enemy_type() -> String:
 	var rng_number: int = rng.randi_range(0, 10)
 	if rng_number >= 4 and rng_number < 8:
 		return "ghost"
-	if rng_number >= 8 and rng_number < 9:
+	if rng_number >= 8 and rng_number <= 9:
 		return "minotaur"
-	if rng_number >= 9:
+	if rng_number > 9:
 		return "mage"
 	return "skeleton"
 
