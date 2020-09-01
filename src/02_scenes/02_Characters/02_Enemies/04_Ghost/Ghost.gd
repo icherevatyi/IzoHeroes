2770f9e76d75faeb55245187339eb3c0e7cc25aa
icherevatyi <igor.cherevatyi@gmail.com>
2020-09-01 17:06:00 +0300

    implemented skull projectile attack

--- src/02_scenes/02_Characters/02_Enemies/04_Ghost/Ghost.gd
+++ src/02_scenes/02_Characters/02_Enemies/04_Ghost/Ghost.gd
@@ -1,6 +1,9 @@
 extends "res://src/02_scenes/02_Characters/02_Enemies/Enemy.gd"
 
+var skull_scene: PackedScene = preload("res://src/02_scenes/04_Items/03_EnemyMagic/02_MagicSkull/MagicSkull.tscn")
 onready var hitbox: CollisionShape2D = $HitBox/CollisionShape2D
+onready var skull_spawn: Position2D = $SkullSpawn
+
 
 func _ready():
 	type = "ghost"
@@ -9,3 +12,22 @@ func _ready():
 	
 	emit_signal("initiate_healthpool", health_max)
 	loot_scripts.get_loot_table(type)
+
+
+
+func _get_coords() -> Vector2:
+	for body in attack_range.get_overlapping_bodies():
+		if body.name == "Player":
+			return body.get_global_position()
+		else:
+			return player_coords
+
+
+func _create_skull() -> void:
+	var skull_instance: RigidBody2D = skull_scene.instance()
+	var world_node: Node2D = get_node("/root/Dungeon/YSort")
+	skull_instance.set_global_position(skull_spawn.get_global_position())
+	skull_instance.rotation = get_angle_to(_get_coords())
+#	skull_instance.apply_impulse(Vector2(), Vector2(15, 0))
+	world_node.add_child(skull_instance)
+	
--- src/02_scenes/04_Items/03_EnemyMagic/02_MagicSkull/MagicSkull.gd
+++ src/02_scenes/04_Items/03_EnemyMagic/02_MagicSkull/MagicSkull.gd
@@ -1,5 +1,27 @@
 extends RigidBody2D
 
+signal do_damage(damage)
 
-func _ready():
-	pass
+func _ready() -> void:
+	apply_impulse(Vector2(), Vector2(150, 0).rotated(rotation))
+
+func _on_MagicSkull_body_entered(body) -> void:
+	print(body.name)
+	if body.name == "Player":
+		_connect_signal("do_damage", body, "_damage_taken")
+		emit_signal("do_damage", 1)
+		disconnect("do_damage", body, "_damage_taken")
+		queue_free()
+	if body.is_in_group("rooms"):
+		queue_free()
+
+
+func _connect_signal(signal_title: String, target_node, target_function_title: String) -> void:
+	match is_connected(signal_title, target_node, target_function_title):
+		false:
+			var connection_msg: int = connect(signal_title, target_node, target_function_title)
+			if connection_msg == 0:
+				return
+			else:
+				print("Signal connection error: ", connection_msg)
+
