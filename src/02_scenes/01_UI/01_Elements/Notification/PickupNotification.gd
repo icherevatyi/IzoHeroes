7d4aba21c9548ca0b9d57b2644f2c26ded622235
icherevatyi <igor.cherevatyi@gmail.com>
2020-09-13 20:19:10 +0300

    added FloorMess sprite map, updated blink intensity

--- src/02_scenes/01_UI/01_Elements/Notification/PickupNotification.gd
+++ src/02_scenes/01_UI/01_Elements/Notification/PickupNotification.gd
@@ -9,12 +9,8 @@ onready var tween: Tween = $Tween
 
 
 func _ready() -> void:
-	_tween_response = tween.interpolate_property(self, "modulate", Color(1, 1, 1, 1), Color(1, 1, 1, 0),  2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	_tween_response = tween.interpolate_property(self, "modulate", Color(1, 1, 1, 1), Color(1, 1, 1, 0),  2.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	_tween_response = tween.start()
 	
 	yield(tween, "tween_all_completed")
 	queue_free()
-
-
-func _on_message_shown() -> void:
-	pass
