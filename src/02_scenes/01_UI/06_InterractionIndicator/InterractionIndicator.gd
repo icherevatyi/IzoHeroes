924f1da753e0ffe850fa9ad0691cef0b2327ce10
icherevatyi <igor.cherevatyi@gmail.com>
2020-09-20 19:33:05 +0300

    fixed position of activation line and activation time

--- src/02_scenes/01_UI/06_InterractionIndicator/InterractionIndicator.gd
+++ src/02_scenes/01_UI/06_InterractionIndicator/InterractionIndicator.gd
@@ -8,6 +8,12 @@ var slide_trans_param = Tween.TRANS_BACK
 var start_point: Vector2 = Vector2(0, 13)
 var end_point: Vector2 = Vector2(0, -19)
 
+var line_start: Vector2 = Vector2(0, 3)
+var line_end: Vector2 = Vector2(24, 3)
+var modulation_visible: Color = Color(1, 1, 1, 1)
+var modulation_hidden: Color = Color(1, 1, 1, 0)
+var activation_time: float = 0.6
+
 
 onready var parent: Node2D = get_parent()
 
@@ -23,8 +29,8 @@ func _ready() -> void:
 	button_label.modulate = Color(1, 1, 1, 0)
 	button_label._set_position(Vector2(0, 13))
 	
-	activation_slider.modulate = Color(1, 1, 1, 1)
-	activation_slider._set_size(Vector2(0, 3))
+	activation_slider.modulate = modulation_hidden
+	activation_slider._set_size(line_start)
 
 
 func _on_indicator_enabled() -> void:
@@ -42,19 +48,19 @@ func _on_indicator_disabled() -> void:
 
 
 func _on_activation_started() ->  void:
-	activation_slider.modulate = Color(1, 1, 1, 1)
-	_response = activation_animation_tween.interpolate_property(activation_slider, "rect_size", Vector2(0, 3), Vector2(40, 3), 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	activation_slider.modulate = modulation_visible
+	_response = activation_animation_tween.interpolate_property(activation_slider, "rect_size", line_start, line_end, activation_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	_response = activation_animation_tween.resume_all()
 	_response = activation_animation_tween.start()
 
 
 func _on_activation_stopped() -> void:
-	activation_slider.modulate = Color(1, 1, 1, 0)
-	activation_slider._set_size(Vector2(0, 3))
+	activation_slider.modulate = modulation_hidden
+	activation_slider._set_size(line_start)
 	_response = activation_animation_tween.stop(activation_slider)
 
 
 func _on_ActivationAnimationTween_tween_all_completed() -> void:
 	parent.activate()
-	activation_slider.modulate = Color(1, 1, 1, 0)
-	activation_slider._set_size(Vector2(0, 3))
+	activation_slider.modulate = modulation_hidden
+	activation_slider._set_size(line_start)
