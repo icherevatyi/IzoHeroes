8ece6231663c8f5d57ca6191bb081905d8ea5d2d
icherevatyi <igor.cherevatyi@gmail.com>
2021-03-06 23:09:12 +0200

    added main menu theme

--- src/02_scenes/01_UI/03_Menus/Menu.gd
+++ src/02_scenes/01_UI/03_Menus/Menu.gd
@@ -1,9 +1,12 @@
 extends CanvasLayer
 
+var _funcion_response: int
 var menu_item: PackedScene = preload("res://src/02_scenes/01_UI/03_Menus/01_MenuItem/MenuItem.tscn")
 
 onready var background: ColorRect = $Background
 onready var items_container: VBoxContainer = $MenuBg/ItemsContainer
+onready var audio_player: AudioStreamPlayer = $AudioStreamPlayer
+onready var audio_tween: Tween = $AudioTween
 
 
 func _ready() -> void:
@@ -22,6 +25,7 @@ func _ready() -> void:
 
 func _start_main_menu() -> void:
 	background.color = Color(0, 0, 0, 1)
+	_start_music()
 	for item_index in Lists.main_menu_list:
 		var item_instance: TextureButton = menu_item.instance()
 		var item_title: String = Lists.main_menu_list[item_index].title
@@ -29,6 +33,13 @@ func _start_main_menu() -> void:
 		
 		items_container.add_child(item_instance)
 		item_instance.item_init(item_title, item_action)
+
+
+func _start_music() -> void:
+	audio_player.playing = true
+	_funcion_response = audio_tween.interpolate_property(audio_player, "volume_db", -80, 0, 0.5, Tween.EASE_IN, Tween.TRANS_LINEAR)
+	_funcion_response = audio_tween.start()
+
 
 
 func _start_deathscreen_menu() -> void:
